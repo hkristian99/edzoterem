278c4cb615313319863b83ff8aabe4ded59cb1d2
hkristian99 <krisztian0424@gmail.com>
2021-11-04 15:04:46 +0000

    Create user, post tables

---
+++ database/migrations/2021_11_04_144421_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->string("slug");
+            $table->string("lead");
+            $table->longText("body");
+            $table->string("cover");
+            $table->foreignId("user_id")->constrained();
+            $table->integer('counter');
+            $table->foreignId("post_status_id")->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
